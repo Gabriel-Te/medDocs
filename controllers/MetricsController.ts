361375f7b7592c2f9d62fe040d52987a1c26dee8
Gabriel-Te <gabrielteixeiracorrea5@gmail.com>
2026-03-03 21:42:59 -0300

    crud de metricas feito com condicionais nos métodos get

--- controllers/MetricsController.ts
+++ controllers/MetricsController.ts
@@ -4,7 +4,7 @@ import { IPrismaMetricsModel } from "../models/interfaces/IPrismaMetricsModel.ts
 
 export class MetricsController {
 
-    public prismaMetricsModel : IPrismaMetricsModel = new(PrismaMetricsModel);
+    public prismaMetricsModel: IPrismaMetricsModel = new (PrismaMetricsModel);
 
     async handle(req: Request, res: Response) {
         try {
@@ -14,6 +14,84 @@ export class MetricsController {
             return res.status(201).json({
                 message: "Sucesso ao criar a métrica",
                 metrics
+            })
+        } catch (error: any) {
+            return res.status(500).json({
+                message: "Erro ao criar a métrica",
+                exc: error.message
+            })
+        }
+    }
+
+    async getAll(req: Request, res: Response) {
+        try {
+            const metrics = await this.prismaMetricsModel.findAll();
+
+            if (!metrics || metrics.length === 0) {
+                return res.status(404).json({
+                    message: "Nenhuma métrica encontrada"
+                })
+            }
+
+            return res.status(200).json({
+                message: "Sucesso ao encontrar as métricas",
+                metrics
+            })
+        } catch (error: any) {
+            return res.status(500).json({
+                message: "Erro ao encontrar as métricas",
+                exc: error.message
+            })
+        }
+    }
+
+    async getById(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+            const metric = await this.prismaMetricsModel.findById(id);
+
+            if (!metric) {
+                return res.status(404).json({
+                    message: "Métrica não encontrada"
+                })
+            }
+            return res.status(200).json({
+                message: "Sucesso ao encontrar a métrica",
+                metric
+            })
+        } catch (error: any) {
+            return res.status(500).json({
+                message: "Erro ao encontrar a métrica",
+                exc: error.message
+            })
+        }
+    }
+
+    async remove(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+            const deletedMetric = await this.prismaMetricsModel.remove(id);
+            return res.status(200).json({
+                message: "Sucesso ao deletar a métrica",
+                deletedMetric
+            })
+        } catch (error: any) {
+            return res.status(500).json({
+                message: "Erro ao deletar a métrica",
+                exc: error.message
+            })
+        }
+    }
+
+    async edit(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id);
+            const metricsForm = req.body;
+            console.log(metricsForm)
+            const EditedMetric = await this.prismaMetricsModel.edit(id, metricsForm);
+            return res.status(201).json({
+                message: "Sucesso ao editar a métrica",
+                EditedMetric
             })
         } catch (error: any) {
             return res.status(500).json({
--- routes/metricsRouter.ts
+++ routes/metricsRouter.ts
@@ -5,6 +5,10 @@ const metricsRouter = Router();
 
 const metricsController = new MetricsController();
 
-metricsRouter.post("/create", metricsController.handle.bind(metricsController))
+metricsRouter.post("/create", metricsController.handle.bind(metricsController));
+metricsRouter.get("/", metricsController.getAll.bind(metricsController));
+metricsRouter.get("/:id", metricsController.getById.bind(metricsController));
+metricsRouter.delete("/:id", metricsController.remove.bind(metricsController));
+metricsRouter.put("/:id", metricsController.edit.bind(metricsController));
 
 export default metricsRouter;
